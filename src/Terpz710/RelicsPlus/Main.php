354e5e8e1dfd75fe3374ba7b1ab9f2c1266346b2
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-10-14 19:53:03 -0700

    Update Main.php

--- src/Terpz710/RelicsPlus/Main.php
+++ src/Terpz710/RelicsPlus/Main.php
@@ -8,15 +8,12 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use Terpz710\RelicsPlus\EventListener\EventListener;
 use Terpz710\RelicsPlus\Commands\RelicsCommand;
-use Terpz710\RelicsPlus\RelicsManager;
 
 class Main extends PluginBase {
 
     public function onEnable(): void {
-        $relicsManager = new RelicsManager();
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
 
-        $this->getServer()->getPluginManager()->registerEvents(new EventListener($relicsManager), $this);
-
-        $this->getServer()->getCommandMap()->register("relics", new RelicsCommand($this, $relicsManager));
+        $this->getServer()->getCommandMap()->register("relics", new RelicsCommand($this));
     }
 }
